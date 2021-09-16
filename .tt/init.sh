f9ad52f4dad9d8c83b2a491c08520545058e1217
Nicola Tomassoni <n.tomassoni@studio-m3.it>
2021-09-16 12:10:56 +0200

    Sudo non utilizzabile

--- .tt/init.sh
+++ .tt/init.sh
@@ -9,41 +9,41 @@ clear
 echo -e "--------------------------------------"
 echo -e "--- TourTools: avvio init composer ---"
 
-sudo -u www-data composer create-project drupal/recommended-project $TMPPATH --no-interaction
-sudo -u www-data cp -Rp /tmp/drupal-composer-tmp/* $SCRIPTPATH/
+composer create-project drupal/recommended-project $TMPPATH --no-interaction
+cp -Rp /tmp/drupal-composer-tmp/* $SCRIPTPATH/
 rm -Rf $SCRIPTPATH/.git
 rm -Rf $TMPPATH
 
-sudo -u www-data composer config repositories.ttf6_paragraphs vcs "https://tourtools@bitbucket.org/tourtoolsteam/ttf6_paragraphs.git"
+composer config repositories.ttf6_paragraphs vcs "https://tourtools@bitbucket.org/tourtoolsteam/ttf6_paragraphs.git"
 
-sudo -u www-data composer require 'drupal/adminimal_theme' 'drupal/adminimal_admin_toolbar' 'drupal/module_filter'
-sudo -u www-data composer require 'drupal/crop' 'drupal/image_widget_crop' 'drupal/metatag' 'drupal/simple_sitemap'
-sudo -u www-data composer require 'drupal/menu_position:^1.0@beta' 'drupal/menu_block' 'drupal/menu_link_attributes'
-sudo -u www-data composer require 'drupal/paragraphs' 'drupal/paragraphs_asymmetric_translation_widgets:^1.0@beta' 'drupal/swiftmailer'
-sudo -u www-data composer require 'drupal/twig_field_value' 'drupal/twig_tweak' 'drupal/imce' 'drupal/linkit:^6.0@beta' 'drupal/field_group'
-sudo -u www-data composer require 'drupal/webform' 'drupal/antibot' 'drupal/devel' 'drupal/pathauto' 'drupal/redirect' 'drupal/block_class'
-sudo -u www-data composer require 'drupal/viewsreference' 'drupal/video_embed_field' 'drupal/block_field:^1.0@RC' 'drupal/google_tag'
-sudo -u www-data composer require 'cweagans/composer-patches' 'tourtoolsteam/ttf6_paragraphs:dev-master' 'drush/drush'
+composer require 'drupal/adminimal_theme' 'drupal/adminimal_admin_toolbar' 'drupal/module_filter'
+composer require 'drupal/crop' 'drupal/image_widget_crop' 'drupal/metatag' 'drupal/simple_sitemap'
+composer require 'drupal/menu_position:^1.0@beta' 'drupal/menu_block' 'drupal/menu_link_attributes'
+composer require 'drupal/paragraphs' 'drupal/paragraphs_asymmetric_translation_widgets:^1.0@beta' 'drupal/swiftmailer'
+composer require 'drupal/twig_field_value' 'drupal/twig_tweak' 'drupal/imce' 'drupal/linkit:^6.0@beta' 'drupal/field_group'
+composer require 'drupal/webform' 'drupal/antibot' 'drupal/devel' 'drupal/pathauto' 'drupal/redirect' 'drupal/block_class'
+composer require 'drupal/viewsreference' 'drupal/video_embed_field' 'drupal/block_field:^1.0@RC' 'drupal/google_tag'
+composer require 'cweagans/composer-patches' 'tourtoolsteam/ttf6_paragraphs:dev-master' 'drush/drush'
 
-sudo -u www-data composer require --dev 'drupal/devel' 'behat/behat' 'drupal/drupal-extension' 'kint-php/kint'
+composer require --dev 'drupal/devel' 'behat/behat' 'drupal/drupal-extension' 'kint-php/kint'
 
 # Site install
 
-sudo -u www-data vendor/bin/drush site:install standard --db-url='mysql://drupal:drupal@db:3306/drupal' --account-name='admin' --account-mail='dev@tourtools.it' --site-mail='dev@tourtools.it' --account-pass='devel' --locale='it' --site-name='test' -y
+vendor/bin/drush site:install standard --db-url='mysql://drupal:drupal@db:3306/drupal' --account-name='admin' --account-mail='dev@tourtools.it' --site-mail='dev@tourtools.it' --account-pass='devel' --locale='it' --site-name='test' -y
 
 # Abilito tramite drush i vari moduli
 echo -e "\n\n\n\n---------------------------------"
 echo -e "--- TourTools: abilito moduli ---"
 
-sudo -u www-data vendor/bin/drush theme:enable -y adminimal_theme
-sudo -u www-data vendor/bin/drush config:set -y system.theme admin adminimal_theme
+vendor/bin/drush theme:enable -y adminimal_theme
+vendor/bin/drush config:set -y system.theme admin adminimal_theme
 
-sudo -u www-data vendor/bin/drush pm-enable -y adminimal_admin_toolbar admin_toolbar_tools module_filter crop image_widget_crop
-sudo -u www-data vendor/bin/drush pm-enable -y metatag simple_sitemap menu_position menu_block menu_link_attributes
-sudo -u www-data vendor/bin/drush pm-enable -y paragraphs paragraphs_asymmetric_translation_widgets swiftmailer
-sudo -u www-data vendor/bin/drush pm-enable -y twig_field_value twig_tweak imce linkit field_group
-sudo -u www-data vendor/bin/drush pm-enable -y webform antibot devel syslog config_translation
-sudo -u www-data vendor/bin/drush pm-enable -y pathauto redirect block_class google_tag
+vendor/bin/drush pm-enable -y adminimal_admin_toolbar admin_toolbar_tools module_filter crop image_widget_crop
+vendor/bin/drush pm-enable -y metatag simple_sitemap menu_position menu_block menu_link_attributes
+vendor/bin/drush pm-enable -y paragraphs paragraphs_asymmetric_translation_widgets swiftmailer
+vendor/bin/drush pm-enable -y twig_field_value twig_tweak imce linkit field_group
+vendor/bin/drush pm-enable -y webform antibot devel syslog config_translation
+vendor/bin/drush pm-enable -y pathauto redirect block_class google_tag
 
 echo -e "\n\n\n\n---------------------------------"
 echo -e "Procedura completata, flush cache"
